6599ccf27d0ebba1c941c013a24b686fb10b180f
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-24 10:10:59 +0200

    Customize App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-const App = () => {
-  return <h1>The Genealogy Project</h1>;
+import React from "react";
+
+const App: React.FC<AppProps> = ({
+  applicationName = "The Genealogy Project",
+}) => {
+  return <h1>{applicationName}</h1>;
 };
 
 export default App;
