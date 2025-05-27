cc09c75bc0fb4616b185265604611e9020ddf074
jbence1994 <juhasz.bence.zsolt@gmail.com>
2025-05-27 12:25:28 +0200

    Make the footer fixed to the bottom of the page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { APPLICATION_NAME } from "../constants/app.constants.ts";
 
 const Footer = () => {
   return (
-    <footer className="text-center py-3 mt-auto">
+    <footer className="text-center py-3 mt-auto fixed-bottom">
       <div className="container">
         <span className="text-muted">
           {APPLICATION_NAME} &copy; {new Date().getFullYear()}
